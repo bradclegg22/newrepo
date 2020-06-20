10a1e838fbde956d4cec45129a6ef31e78bd712a
Brad <cleggbc@byui.edu>
2020-06-20 14:33:35 -0600

    'l'

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ const PORT = process.env.PORT || 5000;
   app.get('/', (req, res) => res.render('/public/static.html'));
   app.get('/calculateRate' calculateRate);
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+  function (req, res)
+{
+    
+}
