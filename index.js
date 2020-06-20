ccd638de0939cbff366b16eef14a52fa1aa78373
Brad <cleggbc@byui.edu>
2020-06-20 14:44:25 -0600

    'l'

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
   app.use(express.static(path.join(__dirname, 'public')));
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'ejs');
+  app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname + '/public/static.html'));
+});
   app.get('/calculateRate', calculateRate);
   app.listen(PORT, function() {
   console.log('Node app is running on port', PORT);
