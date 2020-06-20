b18afce123addccc83ba1d2197cac1ee03e582d8
Brad <cleggbc@byui.edu>
2020-06-20 14:41:57 -0600

    'l'

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-  app.use(express.static(__dirname + '/public'));
-  app.set('views', __dirname + '/views');
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'ejs');
   app.get('/calculateRate', calculateRate);
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  app.listen(PORT, function() {
+  console.log('Node app is running on port', PORT);
+});
 
   function calculateRate (req, res)
 {
